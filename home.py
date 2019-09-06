04cbd8299650c6d52a26c105cd7da9e07a40a428
D4SK <konstantin.vogel@gmx.net>
2019-09-06 16:12:19 +0200

    added buttons

--- home.py
+++ home.py
@@ -6,3 +6,35 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.properties import ListProperty, StringProperty, ObjectProperty
 from kivy.clock import Clock
+from kivy.uix.widget import Widget
+
+class Btn_Double(Widget):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if touch.x-self.x<self.width/2:
+                self.pressed1 = True
+            else:
+                self.pressed2 = True
+            return True
+        return False
+    def on_touch_up(self, touch):
+        if self.pressed1 or self.pressed2:
+            self.pressed1 = self.pressed2 =  False
+            return True
+        return False
+class Btn_Triple(Widget):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if touch.x-self.x<self.w1:
+                self.pressed1 = True
+            elif touch.x-self.x>self.w1 and touch.x-self.x<(self.w1+2+self.w2):
+                self.pressed2 = True
+            else:
+                self.pressed3 = True
+            return True
+        return False
+    def on_touch_up(self, touch):
+        if self.pressed1 or self.pressed2 or self.pressed3:
+            self.pressed1 = self.pressed2 = self.pressed3 = False
+            return True
+        return False
--- parameters.py
+++ parameters.py
@@ -13,4 +13,14 @@ background = [0.1,0.1,0.1,1]
 medium_gray = [0.2,0.2,0.2,1]
 medium_light_gray = [0.4,0.4,0.4,1]
 light_gray = [0.7,0.7,0.7,1]
-translucent_white = 1,1,1, 0.1
+translucent_white = 1,1,1,0.1
+red = 1,0,0,1
+
+
+#Kivy Guide
+#LABELS: try setting hints to None if it does shit e.g. setting size doesnt work
+    # size: outer dimensions of the label widget, available space, can be set to texture_size to show everything 
+    # text_size: can be set to limit texture size e.g. cut off text, can be set to size to show all that fits, 
+    # texture_size: size of the actual text not cut off(outer dimensions), can set font_size
+# in canvas: e.g. self.*** acceses the 'parent' widget of the canvas, unlike in other child Instances like Label:
+# pos: coordinates are always relative to the innermost Layout, not Widget you are in
