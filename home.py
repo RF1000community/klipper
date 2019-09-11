9a5e72dfa8b4e2c697a9f6ea91e3446e7d63c9f9
D4SK <konstantin.vogel@gmx.net>
2019-09-11 02:53:13 +0200

    button refactor, popup

--- home.py
+++ home.py
@@ -4,79 +4,37 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
-from kivy.properties import ListProperty, StringProperty, ObjectProperty
+from kivy.properties import ListProperty, StringProperty, ObjectProperty, BooleanProperty
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.uix.behaviors.button import ButtonBehavior
+#  self.ids.big_button.text = "
 
-class ClearRoundButton(ButtonBehavior, Widget):
-    def on_press(self):
-        self.pressed = True
-    def on_release(self):
-        self.pressed = False
-class ClearButton(ButtonBehavior, Widget):
-    def on_press(self):
-        self.pressed = True
-    def on_release(self):
-        self.pressed = False
-class Btn_Stop(ClearRoundButton):
-    pass
-class Btn_Play(ClearRoundButton):
-    pass
-class Btn(ClearRoundButton):
-    pass
-class Btn_Outline(ClearRoundButton):
-    pass
-class Btn_Temp(ClearRoundButton):
-    pass
-
-class Btn_Arrow(ClearRoundButton):
-    pass
-class Btn_Popup(Widget):
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            if touch.x-self.x<self.width/2:
-                self.pressed1 = True
-            else:
-                self.pressed2 = True
-            return True
-        return super(Btn_Popup, self).on_touch_down(touch)
+class BaseButton(ButtonBehavior, Widget):
     def on_touch_up(self, touch):
-        if self.pressed1 or self.pressed2:
-            self.pressed1 = self.pressed2 =  False
+        if self.pressed: 
+            self.pressed = False
             return True
-        return super(Btn_Popup, self).on_touch_up(touch)
+        return super(BaseButton, self).on_touch_up(touch)
+
+class Btn_Stop(BaseButton):
+    pass
+class Btn_Play(BaseButton):
+    pass
+class Btn(BaseButton):
+    pass
+class Btn_Outline(BaseButton):
+    pass
+class Btn_Temp(BaseButton):
+    pass
+class Btn_Arrow(BaseButton):
+    pass
 class BasePopup(Popup):
-    pass
+    def dismiss(self):
+        return
+    def confirm(self):
+        self.dismiss()
 class Btn_Triple(Widget):
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            if touch.x-self.x<self.w1:
-                self.pressed1 = True
-            elif touch.x-self.x>self.w1 and touch.x-self.x<(self.w1+2+self.w2):
-                self.pressed2 = True
-            else:
-                self.pressed3 = True
-            return True
-        return super(Btn_Triple, self).on_touch_down(touch)
-    def on_touch_up(self, touch):
-        if self.pressed1 or self.pressed2 or self.pressed3:
-            self.pressed1 = self.pressed2 = self.pressed3 = False
-            return True
-        return super(Btn_Triple, self).on_touch_up(touch)
+    pass
 class Btn_TripleZ(Widget):
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            if touch.y-self.y<self.h1:
-                self.pressed1 = True
-            elif touch.y-self.y>self.h1 and touch.y-self.y<(self.h1+self.h2):
-                self.pressed2 = True
-            else:
-                self.pressed3 = True
-            return True
-        return super(Btn_TripleZ, self).on_touch_down(touch)
-    def on_touch_up(self, touch):
-        if self.pressed1 or self.pressed2 or self.pressed3:
-            self.pressed1 = self.pressed2 = self.pressed3 = False
-            return True
-        return super(Btn_TripleZ, self).on_touch_up(touch)
+    pass
--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 large_font = 34
-normal_font = 20
+normal_font = 21
 small_font = 17
 extra_small_font = 14
 screen_width = 600
@@ -13,14 +13,14 @@ popup_radius = 12
 
 background = [0.1,0.1,0.1,1]
 popup = 0.15,0.15,0.15,1
-divider = 1,1,1,0.2
+divider = 1,1,1,0.15
 button = 1,1,1,0.1
 button_outline = 1,1,1,0.2
 medium_gray = [0.2,0.2,0.2,1]
 medium_light_gray = [0.35,0.35,0.35,1]
 light_gray = [0.7,0.7,0.7,1]
 translucent_white = 1,1,1,0.1
-accent = [0.2, 0.71, 0.9, 1]
+accent = [0, 0.76, 0.9, 1]
 red = 0.9,0,0,1
 
 
@@ -37,3 +37,4 @@ fc_starting_path = '~/'
 # in canvas: e.g. self.*** acceses the 'parent' widget of the canvas, unlike in other child Instances like Label:
 # pos: coordinates are always relative to the innermost Layout, not Widget you are in
 # Widgets: always define size first then pos at least when using top or right.. x:
+# Never Put comments after canvas: Instruction
