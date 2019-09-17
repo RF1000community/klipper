9440da00d69a49498e7202880b0a057a37cbfeb0
D4SK <konstantin.vogel@gmx.net>
2019-09-18 00:08:10 +0200

    fps

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ Config.set('graphics', 'height', p.screen_height)
 Config.set('graphics', 'fullscreen', 0 if testing else 1)#set to 1 or 'auto' for raspi
 Config.set('kivy', 'keyboard_layout', 'en_US')
 Config.set('kivy','keyboard_mode', 'dock')
+Config.set('graphics','maxfps',30)
 Config.set("kivy", "log_level", "debug")#temporary for debugging
 
 Builder.unload_file(join(kivy_data_dir, 'style.kv'))
