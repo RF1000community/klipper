9408148249dd07d9ff56baaee8b840e314804c3b
D4SK <konstantin.vogel@gmx.net>
2019-09-20 02:28:41 +0200

    new testconfig

--- main.py
+++ main.py
@@ -8,25 +8,21 @@ else:
     testing = False
 import os
 #os.environ['KIVY_WINDOW'] = 'pygame' 
-#os.environ['KCFG_input_device_%(name)s'] = 'probesysfs,provider=mtdev,param=rotation=90,param=invert_y=1'
 from os.path import dirname, join
 from kivy import kivy_data_dir
 from kivy.lang import Builder
 from kivy.app import App
 from kivy.config import Config
-
 from settings import *
 from home import *
 from files import *
 import parameters as p
 from kivy.config import Config
 
-#Config.read('config.ini')
-# set config
+if testing: Config.read('testconfig.ini')
+else: Config.read('config.ini')
 #Config.write()
 
-Config.set('graphics', 'fullscreen', 0 if testing else 1)#set to 1 or 'auto' for raspi
-Config.set("kivy", "log_level", "debug")#temporary for debugging
 Builder.unload_file(join(kivy_data_dir, 'style.kv'))
 Builder.load_file("style.kv")
 
