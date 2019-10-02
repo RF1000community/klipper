7c20bccd8581800717a5e7a9cd2621f799448d27
D4SK <konstantin.vogel@gmx.net>
2019-10-02 20:57:51 +0200

    install gstrreamer

--- scripts/install-kgui.sh
+++ scripts/install-kgui.sh
@@ -36,6 +36,12 @@ install_kgui()
     PKGLIST="libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
     pkg-config libgl1-mesa-dev libgles2-mesa-dev \
     python-setuptools libgstreamer1.0-dev \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-ugly \
+    gstreamer1.0-omx \
+    gscreamer1.0-alsa \
     python-dev libmtdev-dev \
     xclip xsel libjpeg-dev mtdev-tools xorg python-pil xserver-xorg-video-fbturbo network-manager git python-pip"
 
