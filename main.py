a84f56e3bcd84db672bc5aa0785b33a4393918cd
D4SK <konstantin.vogel@gmx.net>
2019-10-26 13:48:10 +0200

    workaround for kivy bug: give absolute path to Config.read so it works when started from any directory

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from kivy.lang import Builder
 from kivy.app import App
 from kivy.config import Config
 from kivy.clock import Clock
+from pathlib import Path  #for testing in python2 use pip install pathlib
 from elements import UltraKeyboard
 from settings import *
 from home import *
@@ -25,8 +26,8 @@ import parameters as p
 import threading
 import logging
 
-if testing: Config.read('testconfig.ini')
-else: Config.read('config.ini')
+if testing: Config.read(str(Path(__file__).resolve().parent) + '/testconfig.ini') #this needs an absolute path otherwise config will only be loaded when working directory is parent directory of main.py
+else:       Config.read(str(Path(__file__).resolve().parent) + '/config.ini')
 
 Builder.unload_file(join(kivy_data_dir, 'style.kv'))
 Builder.load_file("style.kv")
