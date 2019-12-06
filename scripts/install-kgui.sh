972103627d77a9958b267c5a55aa3f895c5906ca
D4SK <konstantin.vogel@gmx.net>
2019-12-06 13:27:54 +0000

    added automatic restarting

--- scripts/install-kgui.sh
+++ scripts/install-kgui.sh
@@ -83,6 +83,8 @@ User=$USER
 Environment=PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 ExecStart=/bin/bash -c "/usr/bin/startx ${PYTHONDIR}/bin/python ${SRCDIR}/klippy/klippy.py ${HOME}/printer.cfg -v -l /tmp/klippy.log"
 Nice=-4
+Restart=always
+RestartSec=10
 
 [Install]
 WantedBy=default.target
