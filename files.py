2a1eba839dbb798cd300eb9213cbc38db44fb209
D4SK <konstantin.vogel@gmx.net>
2020-02-26 00:40:10 +0100

    new design with grid filechooser and new icons

--- files.py
+++ files.py
@@ -8,6 +8,7 @@ from kivy.clock import Clock
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recyclegridlayout import RecycleGridLayout
 from kivy.uix.recycleview import RecycleView
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty, ObjectProperty, NumericProperty, DictProperty, StringProperty, BooleanProperty, OptionProperty
@@ -16,69 +17,12 @@ from elements import *
 import parameters as p
 
 
-class FC(RecycleView):
+class FC(RecycleView): #TODO event drive updating
     path = StringProperty()
-    btn_stack = ObjectProperty()
-    btn_back_visible = BooleanProperty(False)
-    view = OptionProperty('files', options = ['files', 'queue'])
     def __init__(self, **kwargs):
-        self.app = App.get_running_app()
-        self.filament_crossection = 3.141592653 * (self.app.filament_diameter/2.)**2
-        if os.path.exists(p.sdcard_path):
-            self.path = p.sdcard_path
-        else:
-            self.path = "/"
         super(FC, self).__init__(**kwargs)
-        Clock.schedule_once(self.bind_tab, 0)
-        self.load_files()
-    
-    def bind_tab(self, e):
-        tabs = self.app.root.ids.tabs
-        tabs.bind(current_tab=self.control_updating)
-        self.on_view(view = self.view)
-
-    def control_updating(self, instance, tab):
-        if tab == instance.ids.file_tab:
-            self.load_files(in_background = True)
-            Clock.schedule_interval(self.update, 10)
-
-    def update(self, dt):
-        if self.view == 'files':
-            self.load_files(in_background=True)
-
-    def load_files(self, in_background = False):
-        root, _folders, _files = next(os.walk(self.path))
-        # filter usb
-        usb = []
-        if "USB Device" in _folders:
-            _folders.remove("USB Device")
-            # Check if folder is not empty -> a usb stick is plugged in
-            r, d, f = next(os.walk(join(root, "USB Device")))
-            if len(d) + len(f) > 0:
-                usb = [{'name': "USB Device", 'item_type': 'usb', 'path': (join(root, "USB Device")), 'details':""}]
-        folders = [(f, join(root, f)) for f in _folders]
-        # filter files
-        files = []
-        for f in _files:
-            if ".gco" in os.path.splitext(f)[1]:
-                files.append((f, join(root, f)))
-        # sort
-        files = self.modification_date_sort(files)
-        folders = sorted(folders)
-        
-        # generate dicts
-        folders = [{'name': f[0], 'item_type': "folder", 'path': f[1], 'details': ""} for f in folders]
-        files =   [{'name': f[0], 'item_type': "file",   'path': f[1], 'details': self.get_details(f[1])} for f in files]
-
-        self.btn_back_visible = self.path != p.sdcard_path
-
-        new_data = usb + folders + files
-        if self.data != new_data:
-            self.data = new_data
-            self.refresh_from_data()
-            self.view = 'files'
-            if not in_background:
-                self.scroll_y = 1
+        self.app = App.get_running_app()
+        self.load_queue()
 
     def load_queue(self, in_background=False):
         queue = [{'name': "queued{}".format(i), 'details': "", 'item_type': "queue", 'path':""} for i in range(10)]
@@ -87,68 +31,6 @@ class FC(RecycleView):
         if not in_background:
             self.scroll_y = 1
         self.view = 'queue'
-
-    def on_view(self, instance=None, view='files'): #todo trigger this on app.queue change
-        self.btn_stack.clear_widgets()
-        if view == 'files':# and len(self.app.queued_files) > 1:
-            self.btn_stack.add_widget(Btn_Queue(filechooser = self))
-            self.ids.fc_box.selected_nodes = []
-        elif view == 'queue':
-            self.btn_stack.add_widget(Btn_QX(filechooser = self))
-            self.btn_stack.add_widget(Btn_QUp(filechooser = self))
-            self.btn_stack.add_widget(Btn_QDown(filechooser = self))
-            self.btn_back_visible = True
-
-    def back(self):
-        if self.view == 'files':
-            self.path = dirname(self.path)
-        self.load_files()
-    
-    def modification_date_sort(self, files):
-        return sorted((f for f in files), key=lambda f: os.path.getmtime(f[1]), reverse=True)
-    
-    def get_details(self, path):
-        # Pass the filepath. Returns the filament use of a gcode file 
-        # Return value is shown below Name of each file
-        filament = [
-            r'Ext.*=.*mm',                          # Kisslicer
-            r';.*filament used =',                  # Slic3r
-            r';.*Filament length: \d+.*\(',         # S3d
-            r'.*filament\sused\s=\s.*mm',           # Slic3r PE
-            r';Filament used: \d*.\d+m',            # Cura
-            r';Material#1 Used:\s\d+\.?\d+',        # ideamaker
-            r'.*filament\sused\s.mm.\s=\s[0-9\.]+'  # PrusaSlicer 
-            ]
-        nlines = 100
-        head = tail = []
-        with open(path, 'rb') as gcode_file :
-            # Read first 100 lines from beginning
-            head = [gcode_file.readline() for i in range(nlines)]
-            tail = []
-            # Read further back until there are enough lines
-            block_count = -1
-            while len(tail) < nlines:
-                offset = block_count * 1024
-                try:
-                    gcode_file.seek(offset, os.SEEK_END)
-                    tail = gcode_file.readlines()
-                except: # For the unlikely case that the file is too small
-                    break
-                block_count -= 1
-        tail = tail[-100:]
-
-        for line in (head + tail):
-            for i, regex in enumerate(filament):
-                match = re.search(regex, line)
-                if match:
-                    match2 = re.search(r'\d*\.\d*', match.group())
-                    if match2:
-                        filament = float(match2.group())
-                        if i == 4:
-                            filament *= 1000 # Cura gives meters -> convert to mm
-                        weight = self.filament_crossection*filament*0.0011 #density in g/mm^3
-                        return "{:4.0f}g".format(weight)
-        return ""
 
     def send_queue(self):
         """
@@ -234,6 +116,161 @@ class FCItem(RecycleDataViewBehavior, Label):
         # Respond to the selection of items in the view
         self.selected = is_selected
 
+class GC(RecycleView):
+    path = StringProperty()
+    btn_back_visible = BooleanProperty(False)
+    def __init__(self, **kwargs):
+        self.app = App.get_running_app()
+        self.filament_crossection = 3.141592653 * (self.app.filament_diameter/2.)**2
+        if os.path.exists(p.sdcard_path):
+            self.path = p.sdcard_path
+        else:
+            self.path = "/"
+        super(GC, self).__init__(**kwargs)
+        Clock.schedule_once(self.bind_tab, 0)
+        self.load_files()
+
+    def bind_tab(self, e):
+        tabs = self.app.root.ids.tabs
+        tabs.bind(current_tab=self.control_updating)
+
+    def control_updating(self, instance, tab):
+        if tab == instance.ids.file_tab:
+            self.load_files(in_background = True)
+            Clock.schedule_interval(self.update, 10)
+
+    def update(self, dt):
+        self.load_files(in_background=True)
+
+    def load_files(self, in_background = False):
+        root, _folders, _files = next(os.walk(self.path))
+        # filter usb
+        usb = []
+        if "USB Device" in _folders:
+            _folders.remove("USB Device")
+            # Check if folder is not empty -> a usb stick is plugged in
+            r, d, f = next(os.walk(join(root, "USB Device")))
+            if len(d) + len(f) > 0:
+                usb = [{'name': "USB Device", 'item_type': 'usb', 'path': (join(root, "USB Device")), 'details':""}]
+        folders = [(f, join(root, f)) for f in _folders]
+        # filter files
+        files = []
+        for f in _files:
+            if ".gco" in os.path.splitext(f)[1]:
+                files.append((f, join(root, f)))
+        # sort
+        files = self.modification_date_sort(files)
+        folders = sorted(folders)
+
+        # generate dicts
+        folders = [{'name': f[0], 'item_type': "folder", 'path': f[1], 'details': ""} for f in folders]
+        files =   [{'name': f[0], 'item_type': "file",   'path': f[1], 'details': self.get_details(f[1])} for f in files]
+
+        self.btn_back_visible = self.path != p.sdcard_path
+
+        new_data = usb + folders + files
+        if self.data != new_data:
+            self.data = new_data
+            self.refresh_from_data()
+            self.view = 'files'
+            if not in_background:
+                self.scroll_y = 1
+
+    def back(self):
+        self.path = dirname(self.path)
+        self.load_files()
+    
+    def modification_date_sort(self, files):
+        return sorted((f for f in files), key=lambda f: os.path.getmtime(f[1]), reverse=True)
+    
+    def get_details(self, path):
+        # Pass the filepath. Returns the filament use of a gcode file 
+        # Return value is shown below Name of each file
+        filament = [
+            r'Ext.*=.*mm',                          # Kisslicer
+            r';.*filament used =',                  # Slic3r
+            r';.*Filament length: \d+.*\(',         # S3d
+            r'.*filament\sused\s=\s.*mm',           # Slic3r PE
+            r';Filament used: \d*.\d+m',            # Cura
+            r';Material#1 Used:\s\d+\.?\d+',        # ideamaker
+            r'.*filament\sused\s.mm.\s=\s[0-9\.]+'  # PrusaSlicer 
+            ]
+        nlines = 100
+        head = tail = []
+        with open(path, 'rb') as gcode_file :
+            # Read first 100 lines from beginning
+            head = [gcode_file.readline() for i in range(nlines)]
+            tail = []
+            # Read further back until there are enough lines
+            block_count = -1
+            while len(tail) < nlines:
+                offset = block_count * 1024
+                try:
+                    gcode_file.seek(offset, os.SEEK_END)
+                    tail = gcode_file.readlines()
+                except: # For the unlikely case that the file is too small
+                    break
+                block_count -= 1
+        tail = tail[-100:]
+
+        for line in (head + tail):
+            for i, regex in enumerate(filament):
+                match = re.search(regex, line)
+                if match:
+                    match2 = re.search(r'\d*\.\d*', match.group())
+                    if match2:
+                        filament = float(match2.group())
+                        if i == 4:
+                            filament *= 1000 # Cura gives meters -> convert to mm
+                        weight = self.filament_crossection*filament*0.0011 #density in g/mm^3
+                        return "{:4.0f}g".format(weight)
+        return ""
+
+class GCGrid(LayoutSelectionBehavior, RecycleGridLayout):
+    # Adds selection behaviour to the view
+    pass
+
+class GCItem(RecycleDataViewBehavior, Label):
+    item_type = OptionProperty('file', options = ['file', 'folder', 'usb'])
+    name = StringProperty()
+    path = StringProperty()
+    details = StringProperty()
+    index = None
+    pressed = BooleanProperty(False)
+
+    def refresh_view_attrs(self, rv, index, data):
+        # Catch and handle the view changes
+        self.index = index
+        return super(GCItem, self).refresh_view_attrs(rv, index, data)
+
+    def on_touch_down(self, touch):
+        # Add selection on touch down
+        if super(GCItem, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos):
+            self.pressed = True
+            return True
+
+    def on_touch_up(self, touch):
+        was_pressed = self.pressed
+        self.pressed = False
+        if super(GCItem, self).on_touch_up(touch):
+            return True
+        if self.collide_point(*touch.pos) and was_pressed:
+            gc = self.parent.parent
+            if self.item_type == 'file':
+                self.popup = PrintPopup(self.path, filechooser=gc)
+                self.popup.open()
+            elif self.item_type == 'folder' or self.item_type == 'usb':
+                gc.path = self.path
+                gc.load_files()
+            return True
+        return False
+
+    def apply_selection(self, rv, index, is_selected):
+        # Respond to the selection of items in the view
+        self.selected = is_selected
+
 class PrintPopup(BasePopup):
 
     def __init__(self, path, filechooser, **kwargs):
@@ -254,25 +291,12 @@ class PrintPopup(BasePopup):
         tabs = app.root.ids.tabs
         tabs.switch_to(tabs.ids.home_tab)
 
-
     def delete(self):
         """Open a confirmation dialog to delete the file"""
         super(PrintPopup, self).dismiss() # dismiss bypassing deselection
         self.confirm_del = DelPopup(path = self.path, filechooser=self.filechooser)
         self.confirm_del.open()
 
-class FloatingButton(BaseButton):
-    def __init__(self, filechooser, **kwargs):
-        self.filechooser = filechooser
-        super(FloatingButton, self).__init__(**kwargs)
-class Btn_Queue(FloatingButton):
-    pass
-class Btn_QUp(FloatingButton):
-    pass
-class Btn_QDown(FloatingButton):
-    pass
-class Btn_QX(FloatingButton):
-    pass
 
 class DelPopup(BasePopup):
     """Popup to confirm file deletion"""
