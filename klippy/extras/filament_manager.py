7ed435ec7c1d5c91593c3ea2825c87f8c0616917
D4SK <konstantin.vogel@gmx.net>
2020-03-10 01:14:25 +0100

    added json loading for filament manager

--- klippy/extras/filament_manager.py
+++ klippy/extras/filament_manager.py
@@ -1,8 +1,11 @@
+import os
+import json
+import logging
+
 from xml.etree import cElementTree #just ElementTree for py3
 from os import walk
-from os.path import expanduser
-import os
-import logging
+from os.path import expanduser, join
+
 from pytictoc import TicToc
 
 class FilamentManager:
@@ -19,37 +22,41 @@ class FilamentManager:
             self.min_path_len = self.filament_manager_config.getfloat('min_path_length')
             self.printer.register_event_handler("klippy:connect", self.handle_connect)
 
-        self.materials_path = expanduser('~/materials')
-        
-        self.materials = []
-        self.loaded_materials = []
+        self.material_dir = expanduser('~/materials')
+        self.loaded_material_path = join(self.material_dir, "loaded_material.json")
+
+        self.loaded_material = []
         self.my_materials = []
         self.tmc_to_guid = {} # Type -> Manufacturer -> Color
         self.guid_to_path = {}
 
-        self.parse_xml()
+        self.read_xml_material_library()
 
     def handle_connect(self):
         #get Extruders from Printer
         self.extruders = []
-        for i in range(0,10):
+        for i in range(10):
             ext = self.printer.lookup_object('extruder{}'.format('' if i==0 else i), None)
-            if ext: self.extruders.append(ext)
+            if ext: 
+                self.extruders.append(ext)
+            else:
+                break
 
-    def parse_xml(self):
+
+    ######## manage cura-material xml files
+    def read_xml_material_library(self):
         t = TicToc()
         t.tic()
         self.guid_to_path = {}
         self.tmc_to_guid = {}
-        if os.path.exists(self.materials_path):
-            root, dirs, files = next(walk(self.materials_path))
+        if os.path.exists(self.material_dir):
+            root, dirs, files = next(walk(self.material_dir))
             for f in files:
-                f_path = os.path.join(self.materials_path, f)
-                # iterparse implementaion is actually slower, so parse whole file in C
+                f_path = os.path.join(self.material_dir, f)
                 try:
-                    f_root = cElementTree.parse(os.path.join(self.materials_path, f)).getroot()
+                    f_root = cElementTree.parse(f_path).getroot()
                 except: 
-                    print("fail {}".format(f))
+                    print("failed to parse xml-material-file {}".format(f_path))
                     continue
                 f_metadata = f_root.find('{http://www.ultimaker.com/material}metadata')
                 f_name = f_metadata.find('{http://www.ultimaker.com/material}name')
@@ -70,7 +77,7 @@ class FilamentManager:
         t.toc()
         logging.info("time to parse: {}".format(t.elapsed))
 
-    def get_text(self, path, tags):
+    def get_text_from_path(self, path, tags):
         try:
             current_tag = cElementTree.parse(path).getroot()
         except: 
@@ -80,10 +87,15 @@ class FilamentManager:
             current_tag = current_tag.find('{http://www.ultimaker.com/material}'+tag)
         return current_tag.text
 
+    def get_text_from_tool(self, tool_id, tags):
+        pass
+
+
+    ######## loading and unloading api
     def get_status(self):
         pass
 
-    def load(self, tool_id):
+    def load(self, tool_id, material_guid):
         pass
 
     def unload(self, tool_id):
@@ -91,6 +103,36 @@ class FilamentManager:
 
     def stop(self, tool_id):
         pass
+
+
+    
+    ######## store json for loaded and recently unloaded materials and their amount
+    def read_loaded_material(self):
+        """Read the material file and return it as a list object"""
+        try:
+            with open(self.loaded_material_path, "r") as f:
+                material = json.load(f)
+        except (IOError, ValueError): # No file or incorrect JSON
+            logging.info("Filament-Manager: Couldn't read loaded-material-file at " + self.loaded_material_path)
+            material = {'loaded':[], 'unloaded':[]}
+        return material
+
+    def verify_json_materials(self, materials):
+        """Only return True when the entire file has a correct structure"""
+        pass
+
+    def write(self, materials):
+        """Write the object to the history file"""
+        try:
+            with open(p.history_file, "w") as f:
+                json.dump(materials, f, indent=True)
+        except IOError:
+            return
+
+    def add(self, path, status):
+        """Add a new entry to the history with the path and status string specified"""
+        self.history.append([path, status, time.time()])
+        self.write(self.history)
         
 def load_config(config):
     return FilamentManager(config)
