34ce4be9c9e7593c3ec9412505798fa5b50524a5
D4SK <konstantin.vogel@gmx.net>
2020-05-26 13:58:15 +0200

    finished ui for updating, rearranged shutdown buttons change flow tuning to 0.5% steps

--- update.py
+++ update.py
@@ -5,6 +5,7 @@ import site
 import requests
 
 from os.path import dirname
+from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.screenmanager import Screen
 from kivy.properties import NumericProperty
@@ -24,44 +25,38 @@ class UpdateScreen(Screen):
     def draw_releases(self, dt):
         if not "KGUI_TESTING" in os.environ:
             self.ids.message.text = "You're on Version " + get_git_version()
-        response = requests.get("https://api.github.com/repos/D4SK/klipperui/releases")
-        if response.ok:
-            releases = response.json()
-            self.ids.box.clear_widgets()
-            self.ids.box.add_widget(Divider(pos_hint={'center_x':0.5}))
-            self.ids.box.add_widget(SIRelease("master", "latest beta version"))
+        self.ids.box.clear_widgets()
+        self.ids.box.add_widget(Divider(pos_hint={'center_x':0.5}))
+
+        releases = requests.get("https://api.github.com/repos/D4SK/klipperui/releases")
+        if releases.ok:
+            releases = releases.json()
             for release in releases:
                 self.ids.box.add_widget(SIRelease(release['tag_name'], release['published_at'].split("T")[0]))
 
-    def run_command(command, output_property):
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        while True:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
-                break
-            elif output:
-                current = getattr(self, output_property)
-                setattr(self, output_property, current + output)
-        rc = process.poll()
-        return rc
+        branches = requests.get("https://api.github.com/repos/D4SK/klipperui/branches")
+        if branches.ok:
+            branches = branches.json()
+            for branch in branches:
+                self.ids.box.add_widget(SIRelease(branch['name'], "for development only"))
 
 class UpdatePopup(BasePopup):
-    message = StringProperty()
     def __init__(self, version, details, **kwargs):
-        super().__init__(**kwargs)
         self.version = version
         self.details = details
+        super().__init__(**kwargs)
 
     def update(self):
-        pass
-
-    def full_install(self):
         # as a convention klipper is always installed in HOME directory
         install_dir = os.path.expanduser('~')
-      
-    def dismiss(self, **kwargs):
-        super().dismiss(**kwargs)
+        self.run_in_tty("some update script ")
+        # exit klipper to terminal
+        App.get_running_app().quit()
 
+    def run_in_tty(self, command):
+        #TODO
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    
 class SIRelease(SetItem):
     def __init__(self, version, details, **kwargs):
         self.version = version
