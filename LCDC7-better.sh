146ccbbf67073eaad0b2cf229a57f001151ff379
Gabriel Vogel <gabriel.vogel@online.de>
2020-06-09 12:23:10 +0200

    LCD script: fix indentation and cleanup

--- LCDC7-better.sh
+++ LCDC7-better.sh
@@ -9,19 +9,19 @@
 # distributed under the MIT license:
 #
 # MIT License
-# 
+#
 # Copyright (c) 2019 François Schlaefli
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,7 +42,6 @@ usage() {
     echo "  -f | --force                  force"
     echo "  -h | --help                   display help"
     echo "  -r | --rotate <0|90|180|270>  rotate DEG degrees. Assumes 0"
-    exit 1
 }
 
 
@@ -54,23 +53,24 @@ ROTATE=0
 while [ "$1" != "" ]; do
     case $1 in
         -f | --force)
-		FORCE=1
-		;;
-	-u | --uninstall)
-    		UNINSTALL=1
-                ;;
+            FORCE=1
+            ;;
+        -u | --uninstall)
+            UNINSTALL=1
+            ;;
         -h | --help )
-                usage
-	        exit
-	        ;;
+            usage
+            exit
+            ;;
         -r | --rotate)
-                ROTATE=$2
-                shift
-                ;;     
-        * )     
-	    	echo "Invalid parameter $1"
-    		usage
-	    	exit 1
+            # Consume two arguments
+            ROTATE=$2
+            shift
+            ;;
+        *)
+            echo "Invalid parameter $1"
+            usage
+            exit 1
     esac
     shift
 done
@@ -79,86 +79,89 @@ done
 machine_is_raspi=$(grep -c "Raspberry Pi" /proc/device-tree/model)
 if (( ! machine_is_raspi )) && (( $FORCE == 0 ))
 then
-	echo "This script is only compatible with Raspberry Pi."
-        echo "Use -f to ignore this warning and proceed anyway."
-	exit 1
+    echo "This script is only compatible with Raspberry Pi."
+    echo "Use -f to ignore this warning and proceed anyway."
+    exit 1
 fi
 
 old='#OLD_CONFIG_'
 new='#LCD_CUSTOM_CONFIG'
-bootconfig=/boot/config.txt
+bcfile=/boot/config.txt
 
 restore_boot_config() {
-	echo "Restoring $bootconfig values ..."
-	
-	# Remove our config
-	sed -i "/$new$/d" $bootconfig
+    echo "Restoring $bcfile values..."
 
-	# Restore old config
-	sed -i "s/$old//" $bootconfig
+    # Remove our config
+    sed -i "/$new$/d" $bcfile
 
+    # Restore old config
+    sed -i "s/$old//" $bcfile
+
+    if [ -e /etc/X11/xorg.conf.d/40-libinput.conf.OLD_CONFIG ]
+    then
         echo "Restoring 40-libinput.conf..."
         # Restore old libinput.conf
         mv -f /etc/X11/xorg.conf.d/40-libinput.conf.OLD_CONFIG /etc/X11/xorg.conf.d/40-libinput.conf
+    fi
 }
 
 if (( $UNINSTALL == 1 )); then
-	echo "Uninstalling ..."
-	restore_boot_config
-	echo "Uninstall done. A reboot is needed."
-	exit 0
+    echo "Uninstalling..."
+    restore_boot_config
+    echo "Uninstall done. A reboot is needed."
+    exit 0
 fi
 
 # New boot config for LCD display
 declare -A boot_config
 boot_config=(
-        [dtparam=i2c_arm]="on"
-        [dtparam=spi]="on"
-        [enable_uart]="1"
-        [max_usb_current]="1"
-	[hdmi_force_hotplug]="1"
-	[config_hdmi_boost]="7"
-	[hdmi_drive]="1"
-	[hdmi_group]="2"
-	[hdmi_mode]="87"
-	[hdmi_cvt]="1024 600 60 6 0 0 0"
-	[display_hdmi_rotate]="0"
-        [hdmi_blanking]="1"
+    [dtparam=i2c_arm]="on"
+    [dtparam=spi]="on"
+    [enable_uart]="1"
+    [max_usb_current]="1"
+    [hdmi_force_hotplug]="1"
+    [config_hdmi_boost]="7"
+    [hdmi_drive]="1"
+    [hdmi_group]="2"
+    [hdmi_mode]="87"
+    [hdmi_cvt]="1024 600 60 6 0 0 0"
+    [display_hdmi_rotate]="0"
+    [hdmi_blanking]="1"
 )
 
 case $ROTATE in
-        0)
-                MATRIX="1 0 0 0 1 0 0 0 1"
-                ;;
-        90)
-                boot_config[display_hdmi_rotate]="1"
-                MATRIX="0 1 0 -1 0 1 0 0 1"
-                ;;
-        180)
-                boot_config[display_hdmi_rotate]="2"
-                MATRIX="-1 0 1 0 -1 1 0 0 1"
-                ;;
-        270)
-                boot_config[display_hdmi_rotate]="3"
-                MATRIX="0 -1 1 1 0 0 0 0 1"
-                ;;
-        *)
-                echo "Invalid rotation value $ROTATE"
-                usage
-                exit 1
+    0)
+        MATRIX="1 0 0 0 1 0 0 0 1"
+        ;;
+    90)
+        boot_config[display_hdmi_rotate]="1"
+        MATRIX="0 1 0 -1 0 1 0 0 1"
+        ;;
+    180)
+        boot_config[display_hdmi_rotate]="2"
+        MATRIX="-1 0 1 0 -1 1 0 0 1"
+        ;;
+    270)
+        boot_config[display_hdmi_rotate]="3"
+        MATRIX="0 -1 1 1 0 0 0 0 1"
+        ;;
+    *)
+        echo "Invalid rotation value $ROTATE"
+        usage
+        exit 1
 esac
 
 # Create xorg.conf.d folder if not already existing
 if [ ! -d /etc/X11/xorg.conf.d ]
 then
-        mkdir -p /etc/X11/xorg.conf.d
+    mkdir -p /etc/X11/xorg.conf.d
 fi
 
 # Backup 40-libinput.conf
 if [ -e /etc/X11/xorg.conf.d/40-libinput.conf ] && [ ! -e /etc/X11/xorg.conf.d/40-libinput.conf.OLD_CONFIG ]
 then
-        echo "/etc/X11/xorg.conf.d/40-libinput.conf already exists. Creating backup.."
-        cp /etc/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf.OLD_CONFIG
+    echo "/etc/X11/xorg.conf.d/40-libinput.conf already exists. Creating backup.."
+    cp /etc/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf.OLD_CONFIG
 fi
 
 # Write new config, dependent on touchpad transformation Matrix
@@ -187,7 +190,7 @@ EndSection
 Section "InputClass"
         Identifier "libinput touchscreen catchall"
         MatchIsTouchscreen "on"
-	Option "CalibrationMatrix" "$MATRIX"
+        Option "CalibrationMatrix" "$MATRIX"
         MatchDevicePath "/dev/input/event*"
         Driver "libinput"
 EndSection
@@ -201,25 +204,23 @@ EndSection
 EOF
 
 
-if grep -q $new $bootconfig; then
-        echo "LCD configuration found in $bootconfig. Rewriting config..."
-        # Remove our new config, so it won't get saved as old
-        sed -i "/$new$/d" $bootconfig
+if grep -q $new $bcfile
+then
+    echo "LCD configuration found in $bcfile. Rewriting config..."
+    # Remove our new config, so it won't get saved as old
+    sed -i "/$new$/d" $bcfile
 fi
 
-write_boot_config() {
-	echo "Saving $bootconfig current values and writing new config ..."
+# Write the boot config
+echo "Saving $bcfile current values and writing new config..."
 
-	# Backup old config and write new one
-	for i in "${!boot_config[@]}"
-	do
-		key=$i
-		value=${boot_config[$i]}
-		sed -i -r "s/^($key)/$old\1/" $bootconfig
-		echo "$key=$value $new" >> "$bootconfig"
-	done
-}
-
-write_boot_config
+# Backup old config and write new one
+for i in "${!boot_config[@]}"
+do
+    key=$i
+    value=${boot_config[$i]}
+    sed -i -r "s/^($key)/$old\1/" $bcfile
+    echo "$key=$value $new" >> "$bcfile"
+done
 
 echo "Installation done. A reboot is needed to start using your touchscreen"
